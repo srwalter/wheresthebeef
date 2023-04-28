38315210137cc92b9e08fe53408a62d1ab16e4a8
Steven Walter <stevenrwalter@gmail.com>
2023-04-28 10:45:07 -0400

    Support serving static files
    Why is this the hardest part of the project?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 use serde::Deserialize;
 use mysql::prelude::*;
+
+use tokio::fs::File;
+use tokio_util::codec::{BytesCodec, FramedRead};
 
 #[derive(Deserialize)]
 struct SqlRequest {
@@ -31,18 +35,27 @@ fn sql_request(req: SqlRequest) -> String {
 }
 
 async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let body = hyper::body::to_bytes(req.into_body()).await.unwrap();
-    let body_str = String::from_utf8(body.to_vec()).unwrap();
-    println!("body {}", body_str);
-    let sql: SqlRequest = serde_json::from_str(&body_str).unwrap();
-    let resp = sql_request(sql);
-    Ok(Response::new(Body::from(resp)))
+    let mut uri = req.uri().to_string();
+    uri.remove(0); // remove the /
+    println!("uri {}", uri);
+    if uri.starts_with("database") {
+        let body = hyper::body::to_bytes(req.into_body()).await.unwrap();
+        let body_str = String::from_utf8(body.to_vec()).unwrap();
+        println!("body {}", body_str);
+        let sql: SqlRequest = serde_json::from_str(&body_str).unwrap();
+        let resp = sql_request(sql);
+        Ok(Response::new(Body::from(resp)))
+    } else {
+        let file = File::open(uri).await.unwrap();
+        let body = Body::wrap_stream(FramedRead::new(file, BytesCodec::new()));
+        Ok(Response::new(body))
+    }
 }
 
 #[tokio::main]
 async fn main() {
     // Construct our SocketAddr to listen on...
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     // And a MakeService to handle each connection...
     let make_service = make_service_fn(|_conn| async {
