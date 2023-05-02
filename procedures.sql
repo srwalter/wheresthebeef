59af63a39bb68068259fe509b55cae95192a8aee
Steven Walter <stevenrwalter@gmail.com>
2023-05-01 21:53:59 -0400

    procedures.sql: give an indicates that the operation succeeded

--- procedures.sql
+++ procedures.sql
@@ -1,30 +1,33 @@
 DELIMITER //
 
 DROP PROCEDURE IF EXISTS createUser //
-CREATE PROCEDURE createUser (IN username VARCHAR(255), IN password VARCHAR(255))
+CREATE PROCEDURE createUser (IN username VARCHAR(255), IN password VARCHAR(255), OUT result VARCHAR(255))
 BEGIN
     SET @sql = CONCAT('CREATE USER ''', username, '''@''%'' IDENTIFIED BY ''', password, '''');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
+    SET result = "Success";
 END //
 
 DROP PROCEDURE IF EXISTS changePassword //
-CREATE PROCEDURE changePassword (IN password VARCHAR(255))
+CREATE PROCEDURE changePassword (IN password VARCHAR(255), OUT result VARCHAR(255))
 BEGIN
     SET @sql = CONCAT('SET PASSWORD = ''', password, ''';');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
+    SET result = "Success";
 END //
 
 DROP PROCEDURE IF EXISTS grantRole //
-CREATE PROCEDURE grantRole (IN username VARCHAR(255), IN role VARCHAR(255))
+CREATE PROCEDURE grantRole (IN username VARCHAR(255), IN role VARCHAR(255), OUT result VARCHAR(255))
 BEGIN
     SET @sql = CONCAT('GRANT ', role, ' TO ''', username, '''@''%''');
     PREPARE stmt FROM @sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
+    SET result = "Success";
 END //
 
 DELIMITER ;
