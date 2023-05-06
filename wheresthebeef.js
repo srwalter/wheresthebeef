e67f48471aa2f662011c3b1fffd087af779c43d4
Steven Walter <stevenrwalter@gmail.com>
2023-05-06 16:09:55 -0400

    Support hiding columns
    If a column name starts with an underscore, it is hidden by default

--- wheresthebeef.js
+++ wheresthebeef.js
@@ -412,15 +412,21 @@ async function callProcedureFull({proc_name,
             for (let i=0; i < row.length; i++) {
                 const cell = row[i];
 
+                // If the column name starts with an underscore, it should be hidden by default
+                var hidden = false;
+                if (column_names[i][0] == '_') {
+                    hidden = true;
+                }
+
                 var td;
                 if (first) {
                     td = document.createElement('th');
                     td.textContent = get_display_name(output_settings, cell);
-                    set_style_for_element(output_settings, cell, td);
+                    set_style_for_element(output_settings, cell, td, hidden);
                 } else {
                     td = document.createElement('td');
                     td.textContent = cell;
-                    set_style_for_element(output_settings, column_names[i], td);
+                    set_style_for_element(output_settings, column_names[i], td, hidden);
                 }
 
                 tr.appendChild(td);
